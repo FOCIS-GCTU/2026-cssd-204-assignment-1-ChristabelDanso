76d63f9ffadbc870ed335d7e0c623ca6a0359361
Christabel Danso <2425402244@live.gctu.edu.gh>
2026-05-31 18:33:48 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,34 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Christabel Addomaa Danso
+# STUDENT ID: 2425402244
+# Email: 2425402244@live.gctu.edu.gh
+# Grader: Mr. Augustus Buckman
+#
+# On my honor, Christabel Addomaa Danso, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+# Prints my initials as stylized large letters using their uppercase letters.
 
+    print() #blank line
+    print("...CAD")
+    print()
+    print("........CCCCCCC............AAAA............DDDDDDDD.........")  #row 1
+    print("......CCCCCCCCC...........AAAAAA...........DDDDDDDDDD.......")  #row 2
+    print("....CCCC.................AA....AA..........DD.......DDD.....")  #row 3
+    print("...CCCC.................AA......AA.........DD........DD.....")  #row 4
+    print("...CCC.................AAAAAAAAAAAA........DD........DD.....")  #row 5
+    print("...CCC.................AAAAAAAAAAAA........DD........DD.....")  #row 6
+    print("...CCC.................AA........AA........DD........DD.....")  #row 7
+    print("...CCCC................AA........AA........DD.......DDD.....")  #row 8
+    print("....CCCCCCCCCCC...**...AA........AA...**...DDDDDDDDDD.....**")  #row 9
+    print(".....CCCCCCCCCC...**...AA........AA...**...DDDDDDDD.......**")  #row 10
+    print()
 
 
 main()
+
